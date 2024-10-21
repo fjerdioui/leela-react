f44a8cb4e3398effc49e965d3d9030394c908a8c
Face <faycale.jerdioui@gmail.com>
2024-10-21 20:33:50 +0100

    fireBase final

--- functions/src/fire-base-db/scrapers/axiosCheerioScraper.js
+++ functions/src/fire-base-db/scrapers/axiosCheerioScraper.js
@@ -1,4 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+async function scrapeSource2() {
+  const { data } = await axios.get('https://another-example.com/events-london');
+  const $ = cheerio.load(data);
+
+  const events = [];
+  $('.event-item').each((index, element) => {
+    const name = $(element).find('.title').text();
+    const date = $(element).find('.date').text();
+    const venue = $(element).find('.venue').text();
+    const price = $(element).find('.price').text();
+    const ticketLink = $(element).find('.ticket-link').attr('href');
+    events.push({ name, date, venue, price, ticketLink });
+  });
+
+  return events;
+}
+
 module.exports = axiosCheerioScraper;
